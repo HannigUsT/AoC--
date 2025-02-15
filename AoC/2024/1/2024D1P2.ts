5b481827b9b770a90ab92c9648b9277e1385ccd0
HannigUsT <hannibalz322@gmail.com>
2025-02-14 23:26:28 -0300

    day one part 2

---
+++ AoC/2024/1/2024D1P2.ts
@@ -0,0 +1,34 @@
+try {
+  const __dirname = new URL(".", import.meta.url).pathname;
+  const file = await Deno.open(`${__dirname}/input.txt`, { read: true });
+
+  const reader = file.readable.pipeThrough(new TextDecoderStream());
+  const firstList: number[] = [];
+  const secondList: number[] = [];
+
+  for await (const chunk of reader) {
+    const lines = chunk
+      .split("\n")
+      .map((line) => line.trim())
+      .filter(Boolean);
+    for (const line of lines) {
+      const parts = line.split(/\s+/).map(Number);
+      const leftNum = parts[0];
+      const rightNum = parts[1];
+
+      firstList.push(leftNum);
+      secondList.push(rightNum);
+    }
+  }
+  const rightHashMap = new Map<number, number>();
+  for (const num of secondList) {
+    rightHashMap.set(num, (rightHashMap.get(num) || 0) + 1);
+  }
+  let similarityScore = 0;
+  for (const num of firstList) {
+    similarityScore += (rightHashMap.get(num) || 0) * num;
+  }
+  console.log(similarityScore);
+} catch (error) {
+  console.log(error);
+}
